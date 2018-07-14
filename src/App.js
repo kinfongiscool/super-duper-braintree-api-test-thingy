989823c8e75ff27c834c9ea36d12bc6d53a011e0
Kin Fong <bradley.kin.fong@gmail.com>
2018-07-13 20:47:23 -0500

    update text

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
         <div className="DropIn" ref={ ref => (this._wrapper = ref) } />
 				<button onClick={ this.checkOut.bind(this) }>Continue</button>
           <div className="Content">
-            <p>This page is a quick implementation of the Braintree Web Drop In for my application for the API Specialist position listed on the <a href="https://boards.greenhouse.io/braintree/jobs/1141106?gh_jid=1141106">Braintree careers page.</a></p>
+            <p>This page is a quick implementation of the Braintree Web Drop In for my application for the API Specialist position listed on the <a href="https://boards.greenhouse.io/braintree/jobs/1141106?gh_jid=1141106">Braintree careers page</a>.</p>
             <p>In order to complete this project, I used the <a href="https://github.com/braintree/braintree-web-drop-in">Braintree Web Drop In</a>, <a href="https://github.com/facebookincubator/create-react-app">Create React App</a>, and referred to the <a href="https://developers.braintreepayments.com/start/overview">Braintree Developer Docs</a>.</p>
         </div>
       </div>
