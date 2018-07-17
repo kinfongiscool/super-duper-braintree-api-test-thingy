87ce016113c8e0c65338b622ec65598414b5bb3d
Kin Fong <bradley.kin.fong@gmail.com>
2018-07-17 01:37:35 -0500

    remove DropIn add Hosted Fields
    I'm scared because it worked the first time

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 import './App.css';
-import BraintreeWebDropIn from "braintree-web-drop-in";
+import BraintreeWebDropIn from 'braintree-web-drop-in';
+import BraintreeWeb from 'braintree-web';
 
 class App extends Component {
 
-  _wrapper;
+  submitButton;
+  form;
+
+  constructor(props) {
+    super(props);
+
+    this.createHostedFields = this.createHostedFields.bind(this);
+  }
 
   async componentDidMount() {
     this.getClientToken();
-	}
+  }
 
-  async setDropIn(clientToken) {
-    this.instance = await BraintreeWebDropIn.create({
-      container: ReactDOM.findDOMNode(this._wrapper),
-      ...{ authorization: clientToken }
+  async setBraintree(clientToken) {
+    BraintreeWeb.client.create({
+      authorization: clientToken
+    }, (err, clientInstance) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      this.createHostedFields(clientInstance);
+    });
+  }
+
+  createHostedFields(clientInstance) {
+    BraintreeWeb.hostedFields.create({
+      client: clientInstance,
+      styles: {
+        'input': {
+          'font-size': '16px',
+          'color': '#ccc',
+          'font-family': 'Lato',
+        },
+        ':focus': {
+          'color': 'black'
+        },
+        '.valid': {
+          'color': '#40BCD8'
+        }
+      },
+      fields: {
+        number: {
+          selector: '#cardNumber',
+          placeholder: '4111 1111 1111 1111'
+        },
+        expirationDate: {
+          selector: '#expirationDate',
+          placeholder: 'MM/YYYY'
+        },
+        cvv: {
+          selector: '#cvv',
+          placeholder: '123'
+        }
+      }
+    }, (err, hostedFieldsInstance) => {
+      this.instance = hostedFieldsInstance;
+      this.submitButton.removeAttribute('disabled');
+      var teardown = (event) => {
+        event.preventDefault();
+        hostedFieldsInstance.teardown( () => {
+          this.createHostedFields(clientInstance);
+          this.form.removeEventListener('submit', teardown, false);
+        });
+      };
+
+      this.form.addEventListener('submit', teardown, false);
     });
   }
 
@@ -28,38 +86,39 @@ class App extends Component {
         alert('Error getting client token, status code = ' + res.status + ' statusText = ' + res.statusText);
       }
     }).then( (resData) => {
-        if (resData.clientToken) {
-          this.setDropIn(resData.clientToken);
-        } else {
-          alert('Error parsing client token, status code = ' + resData.status + ' statusText = ' + resData.statusText);
-        }
-    }).catch(function (err) {
+      if (resData.clientToken) {
+        this.setBraintree(resData.clientToken);
+      } else {
+        alert('Error parsing client token, status code = ' + resData.status + ' statusText = ' + resData.statusText);
+      }
+    }).catch( (err) => {
       console.error(err);
     })
   }
 
-	async checkOut() {
-    const { nonce } = await this.instance.requestPaymentMethod();
-		await fetch('/check_out', {
-      method: 'POST',
-      body: JSON.stringify({
-        'paymentMethodNonce': nonce
-      }),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }).then( (res) => {
-      console.log("checkOut()" + res)
-
-      if (res.status === 200) {
-        alert('Status code = 200, which means it probably worked!');
-      } else {
-        alert('Error processing payment, status code = ' + res.status + ' statusText = ' + res.statusText);
-      }
-    }).catch(function (err) {
+  async checkOut() {
+    this.instance.tokenize().then( async (payload) => {
+      this.submitButton.setAttribute('disabled', 'disabled');
+      await fetch('/check_out', {
+        method: 'POST',
+        body: JSON.stringify({
+          'paymentMethodNonce': payload.nonce
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then( (res) => {
+        if (res.status === 200) {
+          alert('Status code = 200, which means it probably worked!');
+        } else {
+          alert('Error processing payment, status code = ' + res.status + ' statusText = ' + res.statusText);
+        }
+      })
+    }).catch( (err) => {
+      this.submitButton.removeAttribute('disabled');
       console.error(err);
-    })
-};
+    });
+  };
 
   async componentWillUnmount() {
     if (this.instance) {
@@ -70,11 +129,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="DropIn" ref={ ref => (this._wrapper = ref) } />
-				<button onClick={ this.checkOut.bind(this) }>Continue</button>
-          <div className="Content">
-            <p>This page is a quick implementation of the Braintree Web Drop In for my application for the API Specialist position listed on the <a href="https://boards.greenhouse.io/braintree/jobs/1141106?gh_jid=1141106">Braintree careers page</a>.</p>
-            <p>In order to complete this project, I used the <a href="https://github.com/braintree/braintree-web-drop-in">Braintree Web Drop In</a>, <a href="https://github.com/facebookincubator/create-react-app">Create React App</a>, and referred to the <a href="https://developers.braintreepayments.com/start/overview">Braintree Developer Docs</a>.</p>
+        <div className="hosted-field-form-container" >
+          <form className="hosted-field-form" ref={ ref => (this.form = ref) }>
+            <label className="hosted-field-label" htmlFor="card-number">Card Number</label>
+            <div className="hosted-field" id="cardNumber"/>
+            <label className="hosted-field-label" htmlFor="expirationDate">Expiration Date</label>
+            <div className="hosted-field" id="expirationDate"/>
+            <label className="hosted-field-label" htmlFor="cvv">CVV</label>
+            <div className="hosted-field" id="cvv"/>
+          </form>
+          <button ref={ ref => (this.submitButton = ref) } onClick={ this.checkOut.bind(this) }>Continue</button>
         </div>
       </div>
     );
